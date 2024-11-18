74d1c0451ff69e025ba1eea11dc50274a0ddbe41
Jtoscanoa <jtoscanoa.personal@gmail.com>
2024-11-17 22:22:20 -0500

    Update Control.java

--- src/main/java/taller3/televisores/Control.java
+++ src/main/java/taller3/televisores/Control.java
@@ -2,40 +2,35 @@ package taller3.televisores;
 
 public class Control {
     private TV tv;
-//    public Control(TV tv) {
-//        this.tv = tv;
-//        tv.setControl(this);
-//    }
     public TV getTv() {
         return tv;
     }
     public void setTv(TV tv) {
         this.tv = tv;
-        tv.setControl(this);
     }
     public void turnOn(){
-        tv.turnOn();
+        if (this.tv !=null){tv.turnOn();}
     }
     public void turnOff(){
-        tv.turnOff();
+        if (this.tv !=null){tv.turnOff();}
     }
     public void canalUp(){
-        tv.canalUp();
+        if (this.tv !=null){tv.canalUp();}
     }
     public void canalDown(){
-        tv.canalDown();
+        if (this.tv !=null){tv.canalDown();}
     }
     public void volumenUp(){
-        tv.volumenUp();
+        if (this.tv !=null){tv.volumenUp();}
     }
     public void volumenDown(){
-        tv.volumenDown();
+        if (this.tv !=null){tv.volumenDown();}
     }
     public void setCanal(int canal){
-        tv.setCanal(canal);
+        if (this.tv !=null){tv.setCanal(canal);}
     }
     public void setVolumen(int volumen){
-        tv.setVolumen(volumen);
+        if (this.tv !=null){tv.setVolumen(volumen);}
     }
     public void enlazar(TV tv){
         this.tv = tv;
