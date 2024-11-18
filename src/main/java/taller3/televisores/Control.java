1f8eef9a025219c673cb62f473cdf9a76310fbba
Jtoscanoa <jtoscanoa.personal@gmail.com>
2024-11-17 22:32:48 -0500

    .

--- src/main/java/taller3/televisores/Control.java
+++ src/main/java/taller3/televisores/Control.java
@@ -2,38 +2,65 @@ package taller3.televisores;
 
 public class Control {
     private TV tv;
+
     public TV getTv() {
         return tv;
     }
+
     public void setTv(TV tv) {
         this.tv = tv;
     }
-    public void turnOn(){
-        if (this.tv !=null){tv.turnOn();}
+
+    public void turnOn() {
+        if (this.tv != null) {
+            tv.turnOn();
+        }
     }
-    public void turnOff(){
-        if (this.tv !=null){tv.turnOff();}
+
+    public void turnOff() {
+        if (this.tv != null) {
+            tv.turnOff();
+        }
     }
-    public void canalUp(){
-        if (this.tv !=null){tv.canalUp();}
+
+    public void canalUp() {
+        if (this.tv != null) {
+            tv.canalUp();
+        }
     }
-    public void canalDown(){
-        if (this.tv !=null){tv.canalDown();}
+
+    public void canalDown() {
+        if (this.tv != null) {
+            tv.canalDown();
+        }
     }
-    public void volumenUp(){
-        if (this.tv !=null){tv.volumenUp();}
+
+    public void volumenUp() {
+        if (this.tv != null) {
+            tv.volumenUp();
+        }
     }
-    public void volumenDown(){
-        if (this.tv !=null){tv.volumenDown();}
+
+    public void volumenDown() {
+        if (this.tv != null) {
+            tv.volumenDown();
+        }
     }
-    public void setCanal(int canal){
-        if (this.tv !=null){tv.setCanal(canal);}
+
+    public void setCanal(int canal) {
+        if (this.tv != null) {
+            tv.setCanal(canal);
+        }
     }
-    public void setVolumen(int volumen){
-        if (this.tv !=null){tv.setVolumen(volumen);}
+
+    public void setVolumen(int volumen) {
+        if (this.tv != null) {
+            tv.setVolumen(volumen);
+        }
     }
-    public void enlazar(TV tv){
+
+    public void enlazar(TV tv) {
         this.tv = tv;
-        tv.control = this;
+        tv.setControl(this);
     }
 }
--- src/main/java/taller3/televisores/Marca.java
+++ src/main/java/taller3/televisores/Marca.java
@@ -2,13 +2,16 @@ package taller3.televisores;
 
 public class Marca {
     private String nombre;
-    public Marca(String Nombre) {
-        this.nombre = Nombre;
+
+    public Marca(String nombre) {
+        this.nombre = nombre;
     }
+
     public String getNombre() {
         return nombre;
     }
-    public void setNombre(String Nombre) {
-        this.nombre = Nombre;
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
 }
